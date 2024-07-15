2992fb85411a4d1cd278ef0c7115983a8ea4a865
sungyeopkim11 <777sy777@naver.com>
2024-07-15 17:10:42 +0900

    20240715 자바 첫 수업 내용 커밋

---
+++ 00_자바연습/src/JavaTest.java
@@ -0,0 +1,26 @@
+// 주석은 JS와 동일
+
+//한줄 주석
+/* 범위주석 */
+
+// ctrl , +/- : 휠조정
+public class JavaTest {
+
+	public static void main(String[] args) {
+		System.out.println("이클립스 사용 중");
+        
+        // sysout -> ctrl + space : 자동완성
+		System.out.println("메모장 보다 쉽다");
+        
+		// 실행 : 위에 run 버튼 또는 단축키 = ctrl + f11
+		
+		/* Java API DOC 확인 방법
+		 * - Java API : 자바가 만들어서 제공해준 코드(기능)
+		 * - API DOC : 제공하는 기능에 대한 설명 문서
+		 * 
+		 * 궁금한 것 클릭 -> shfit + f2
+		 * 
+		 */
+	}
+
+}

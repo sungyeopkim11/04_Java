f11e916f7608f4fc50718c52e16fc32ba08e7616
sungyeopkim11 <777sy777@naver.com>
2024-07-22 16:47:19 +0900

    20240722 수업 내용 학생 검색 자바 코드 커밋

---
+++ 06_boject_array/src/student/run/StudentRun.java
@@ -0,0 +1,18 @@
+package student.run;
+
+import student.dto.StudentDTO;
+import student.view.StudentView;
+
+// 실행용 클래스
+public class StudentRun {
+	
+	public static void main(String[] args) {
+		
+		// StudentView 객체 생성
+		StudentView view = new StudentView();
+		view.mainMenu();
+		
+
+	}
+
+}

a5f3284f5b6577f9d14038bc79d0bbe94b3df6df
sungyeopkim11 <777sy777@naver.com>
2024-07-24 16:01:15 +0900

    20240724 수업내용 커밋 상속성 다형성

---
+++ 07_inheritance/src/dto/Child2.java
@@ -0,0 +1,10 @@
+package dto;
+
+public class Child2 extends Parent {
+	
+
+	@Override
+	public String toString() {
+		return "[Child2222]" + super.toString();
+	}
+}
---
+++ 07_inheritance/src/dto/Child3.java
@@ -0,0 +1,10 @@
+package dto;
+
+public class Child3 extends Parent {
+	
+	@Override
+	public String toString() {
+		return "[Child3333]" + super.toString();
+	}
+
+}
